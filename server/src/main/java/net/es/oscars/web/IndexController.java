26f60f685e5ded18ef5c7441e187ea9220af550b
haniotak <haniotak@gmail.com>
2021-02-04 02:42:59 -0800

    heck yea

---
+++ server/src/main/java/net/es/oscars/web/IndexController.java
@@ -0,0 +1,20 @@
+package net.es.oscars.web;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+// https://stackoverflow.com/questions/45147280/
+@Component
+public class IndexController implements WebFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        if (exchange.getRequest().getURI().getPath().equals("/")) {
+            return chain.filter(exchange.mutate().request(exchange.getRequest().mutate().path("/index.html").build()).build());
+        }
+
+        return chain.filter(exchange);
+    }
+}
